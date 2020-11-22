13f258f9772c154d98a16da477a8052d51d87d2c
vishnu <visitvishnuraut@gmail.com>
2020-11-22 23:11:58 +0530

    website updates and 18+ filter

--- clarico_ext/controllers/main.py
+++ clarico_ext/controllers/main.py
@@ -23,24 +23,22 @@ from odoo.addons.website.controllers.main import Website
 	
 @http.route('/', type='http', auth="public", website=True)
 def index(self, **kw):
-    homepage = request.website.homepage_id
-    if homepage and (homepage.sudo().is_visible or request.env.user.has_group('base.group_user')) and homepage.url != '/':
-        return request.env['ir.http'].reroute(homepage.url)
+	homepage = request.website.homepage_id
+	if homepage and (homepage.sudo().is_visible or request.env.user.has_group('base.group_user')) and homepage.url != '/':
+		return request.env['ir.http'].reroute(homepage.url)
 
-    print (999999999999999999999999999999999999)
-    return request.render('clarico_ext.clarico_home', {})
-    
-    website_page = request.env['ir.http']._serve_page()
-    if website_page:
-        print (website_page, 222222222222222222222222222222222222222)
-        return website_page
-    else:
-        top_menu = request.website.menu_id
-        first_menu = top_menu and top_menu.child_id and top_menu.child_id.filtered(lambda menu: menu.is_visible)
-        if first_menu and first_menu[0].url not in ('/', '', '#') and (not (first_menu[0].url.startswith(('/?', '/#', ' ')))):
-            return request.redirect(first_menu[0].url)
+	return request.render('clarico_ext.clarico_home', {})
+	
+	website_page = request.env['ir.http']._serve_page()
+	if website_page:
+		return website_page
+	else:
+		top_menu = request.website.menu_id
+		first_menu = top_menu and top_menu.child_id and top_menu.child_id.filtered(lambda menu: menu.is_visible)
+		if first_menu and first_menu[0].url not in ('/', '', '#') and (not (first_menu[0].url.startswith(('/?', '/#', ' ')))):
+			return request.redirect(first_menu[0].url)
 
-    raise request.not_found()
+	raise request.not_found()
 
 
 Website.index = index	
--- clarico_ext/static/src/js/website_sale.js
+++ clarico_ext/static/src/js/website_sale.js
@@ -1,12 +1,80 @@
-odoo.define('clarico_ext.website_sale', function (require) {
-'use strict';
+odoo.define('clarico_ext.website_sale', function(require) {
+    'use strict';
 
-var core = require('web.core');
-var _t = core._t;
-var ajax = require('web.ajax');
-var wSaleUtils = require('website_sale.utils');
+    var ajax = require('web.ajax');
+    var d = new Date();
 
-var rpc = require('web.rpc');
+
+    
+    if ( sessionStorage.getItem("plus18") + "" == "0" || sessionStorage.getItem("plus18") + "" == 'undefined' || sessionStorage.getItem("plus18") + "" == 'null') {
+        $('#plus18').show();
+    }
+
+
+
+    $('#byear').keyup(function(event) {
+
+        var year = $('#byear').val();
+        var month = $('#bmonth').val();
+        var day = $('#bday').val();
+        if (year.length == 4 && month.length == 2 && day.length == 2) {
+
+            var dateString = month + "/" + day + "/" + year;
+            var dateObject = new Date(dateString);
+            var diff = new Date(d - dateObject);
+            var days = diff / 1000 / 60 / 60 / 24;
+            days = days / 365;
+            if (days > 18) {
+                sessionStorage.setItem("plus18", "1");;
+                $('#plus18').hide();
+            }
+        }
+    });
+
+
+    $('#bmont').keyup(function(event) {
+
+        var year = $('#byear').val()
+        var month = $('#bmonth').val()
+        var day = $('#bday').val()
+
+        if (year.length == 4 && month.length == 2 && day.length == 2) {
+
+            var dateString = month + "/" + day + "/" + year;
+            var dateObject = new Date(dateString);
+            var diff = new Date(d - dateObject);
+            var days = diff / 1000 / 60 / 60 / 24;
+            days = days / 365;
+            if (days > 18) {
+                sessionStorage.setItem("plus18", "1");;
+                $('#plus18').hide();
+            }
+        }
+
+    });
+
+
+    $('#byear').keyup(function(event) {
+
+        var year = $('#byear').val()
+        var month = $('#bmonth').val()
+        var day = $('#bday').val()
+
+        if (year.length == 4 && month.length == 2 && day.length == 2) {
+
+            var dateString = month + "/" + day + "/" + year;
+            var dateObject = new Date(dateString);
+            var diff = new Date(d - dateObject);
+            var days = diff / 1000 / 60 / 60 / 24;
+            days = days / 365;
+            if (days > 18) {
+                sessionStorage.setItem("plus18", "1");;
+                $('#plus18').hide();
+            }
+        }
+
+    });
+
 
 
 
