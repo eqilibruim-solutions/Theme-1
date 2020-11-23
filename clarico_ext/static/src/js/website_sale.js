b18d1791b6f14fb6273929fd3c58105ea1c02bd7
vishnu <visitvishnuraut@gmail.com>
2020-11-23 23:14:48 +0530

    website updates

--- clarico_ext/static/src/js/website_sale.js
+++ clarico_ext/static/src/js/website_sale.js
@@ -5,15 +5,17 @@ odoo.define('clarico_ext.website_sale', function(require) {
     var d = new Date();
 
 
+    console.log(sessionStorage.getItem("plus18"), 'KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK')
     
     if ( sessionStorage.getItem("plus18") + "" == "0" || sessionStorage.getItem("plus18") + "" == 'undefined' || sessionStorage.getItem("plus18") + "" == 'null') {
         $('#plus18').show();
+        $('#bmonth').focus();
     }
 
 
-
-    $('#byear').keyup(function(event) {
-
+    
+    function do_process(e){
+    
         var year = $('#byear').val();
         var month = $('#bmonth').val();
         var day = $('#bday').val();
@@ -25,54 +27,33 @@ odoo.define('clarico_ext.website_sale', function(require) {
             var days = diff / 1000 / 60 / 60 / 24;
             days = days / 365;
             if (days > 18) {
-                sessionStorage.setItem("plus18", "1");;
+                sessionStorage.setItem("plus18", "1");
                 $('#plus18').hide();
             }
         }
+    
+    }
+
+    $('#bday').keyup(function(event) {
+        if ( $('#bday').val().length == 2 )
+        {
+            $('#byear').focus();
+        }
+        do_process();
     });
 
 
-    $('#bmont').keyup(function(event) {
-
-        var year = $('#byear').val()
-        var month = $('#bmonth').val()
-        var day = $('#bday').val()
-
-        if (year.length == 4 && month.length == 2 && day.length == 2) {
-
-            var dateString = month + "/" + day + "/" + year;
-            var dateObject = new Date(dateString);
-            var diff = new Date(d - dateObject);
-            var days = diff / 1000 / 60 / 60 / 24;
-            days = days / 365;
-            if (days > 18) {
-                sessionStorage.setItem("plus18", "1");;
-                $('#plus18').hide();
-            }
+    $('#bmonth').keyup(function(event) {
+        if ( $('#bmonth').val().length == 2 )
+        {
+            $('#bday').focus();
         }
-
+        do_process();
     });
 
 
     $('#byear').keyup(function(event) {
-
-        var year = $('#byear').val()
-        var month = $('#bmonth').val()
-        var day = $('#bday').val()
-
-        if (year.length == 4 && month.length == 2 && day.length == 2) {
-
-            var dateString = month + "/" + day + "/" + year;
-            var dateObject = new Date(dateString);
-            var diff = new Date(d - dateObject);
-            var days = diff / 1000 / 60 / 60 / 24;
-            days = days / 365;
-            if (days > 18) {
-                sessionStorage.setItem("plus18", "1");;
-                $('#plus18').hide();
-            }
-        }
-
+        do_process();
     });
 
 
